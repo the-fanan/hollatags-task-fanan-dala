f0fcf60090e98461e99a1815284fcd6d506d4917
Fanan Dala <fanan.dala@yahoo.com>
2020-03-27 16:31:10 +0100

    feat: added job to bill user

---
+++ app/Jobs/BillUser.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use App\Services\Billing\Billing;
+
+class BillUser implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $biller = new Billing('mock');
+        $biller->chargeCustomer([
+            'amount' => $this->user->amount_to_bill,
+            'mobile_number' => $this->user->mobile_number,
+        ]);
+    }
+
+    /**
+     * The job failed to process.
+     *
+     * @param  Exception  $exception
+     * @return void
+     */
+    public function failed(Exception $exception)
+    {
+        // Send user notification of failure, etc...
+    }
+}
