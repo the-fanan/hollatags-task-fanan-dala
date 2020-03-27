f999eeb906111dcbc9e95469f2ea36a33cb40788
Fanan Dala <fanan.dala@yahoo.com>
2020-03-27 13:40:58 +0100

    config: added users table seeder

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'username', 'mobile_number', 'amount_to_bill'
     ];
 
     /**
@@ -25,7 +25,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        
     ];
 
     /**
